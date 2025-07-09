ef3a1669293708ece147cb5d542f9baa86398455
Oussama Elhousni <oussamaelhousnioe@gmail.com>
2025-07-09 21:30:02 +0100

    add screen wrapper component

---
+++ components/ScreenWrapper.tsx
@@ -0,0 +1,27 @@
+import { colors } from "@/constants/theme";
+import { ScreenWrapperProps } from "@/types";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const ScreenWrapper = ({ style, children }: ScreenWrapperProps) => {
+  const { top: paddingTop } = useSafeAreaInsets();
+
+  return (
+    <View
+      style={[
+        {
+          paddingTop,
+          backgroundColor: colors.neutral900,
+          flex: 1,
+        },
+      ]}
+    >
+      <StatusBar style="light" />
+      {children}
+    </View>
+  );
+};
+
+export default ScreenWrapper;
