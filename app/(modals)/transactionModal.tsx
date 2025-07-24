b04bc0ed1ec37f0aa43639a52cc85b4672249d2b
Oussama Elhousni <oussamaelhousnioe@gmail.com>
2025-07-24 19:45:45 +0100

    add  date picker,amount and category select

--- app/(modals)/transactionModal.tsx
+++ app/(modals)/transactionModal.tsx
@@ -1,21 +1,29 @@
 import BackButton from "@/components/BackButton";
 import Button from "@/components/Button";
 import Header from "@/components/Header";
+import Input from "@/components/Input";
 import ModalWrapper from "@/components/ModalWrapper";
 import Typo from "@/components/Typo";
 import UploadImage from "@/components/UploadImage";
-import { transactionTypes } from "@/constants/data";
+import { expenseCategories, transactionTypes } from "@/constants/data";
 import { colors, radius, spacingX, spacingY } from "@/constants/theme";
 import { useAuth } from "@/contexts/authContext";
 import useFetchData from "@/hooks/useFetchData";
-import { createOrUpdateWallet, deleteWallet } from "@/services/walletService";
+import { deleteWallet } from "@/services/walletService";
 import { TransactionType, WalletType } from "@/types";
 import { verticalScale } from "@/utils/styling";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { orderBy, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import { Alert, StyleSheet, View } from "react-native";
+import {
+  Alert,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { Dropdown } from "react-native-element-dropdown";
 const data = [
   { label: "Item 1", value: "1" },
@@ -33,15 +41,17 @@ const TransactionModal = () => {
 
   const [transaction, setTransaction] = useState<TransactionType>({
     id: "",
-    type: "string",
+    type: "income",
     amount: 0,
-    category: "expense",
+    category: "",
     date: new Date(),
     description: "",
     image: null,
     uid: "string",
     walletId: "",
   });
+
+  const [showDatePicker, setShowDatePicker] = useState(false);
 
   const { data: wallets } = useFetchData<WalletType>("wallets", [
     where("uid", "==", user?.uid),
@@ -60,7 +70,7 @@ const TransactionModal = () => {
   const onSelect = (file: any) => setWalletData({ ...walletData, image: file });
   const onClear = () => setWalletData({ ...walletData, image: null });
   const showDeleteAlert = () => {
-    Alert.alert("Confirm", "Are you sure you want to delete this wallet", [
+    Alert.alert("Confirm", "Are you sure you want to delete this transaction", [
       {
         text: "Cancel",
         style: "cancel",
@@ -81,17 +91,25 @@ const TransactionModal = () => {
     ]);
   };
   const onSubmit = async () => {
-    const { name, image } = walletData;
-    if (!name.trim() || !image) {
-      return Alert.alert("Wallet Error", "Please fill all the fields");
+    const { type, category, walletId, date, description, amount, image } =
+      transaction;
+
+    if (!walletId || !amount || !type || (type === "expense" && !category)) {
+      console.log(transaction);
+      Alert.alert("Transaction error", "Please fill all fields");
+      return;
     }
-    setIsloading(true);
-    const res = await createOrUpdateWallet(walletData);
-    setIsloading(false);
-    if (!res.success) {
-      return Alert.alert("Wallet Error", res.msg);
-    }
-    router.back();
+
+    const transactionData: TransactionType = {
+      uid: user?.uid,
+      type,
+      category,
+      walletId,
+      date,
+      description,
+      amount,
+      image,
+    };
   };
 
   useEffect(() => {
@@ -103,8 +121,6 @@ const TransactionModal = () => {
       });
     }
   }, []);
-  console.log("oldWallet", oldWallet);
-  const [type, setValue] = useState(null);
 
   return (
     <ModalWrapper>
@@ -113,69 +129,175 @@ const TransactionModal = () => {
           title={oldWallet?.id ? "Update Transaction" : "New Transaction"}
           leftIcon={<BackButton />}
         />
-        <View style={styles.form}>
-          <View style={styles.inputContainer}>
-            <Typo size={14} color={colors.neutral300}>
-              Type :
-            </Typo>
-            <Dropdown
-              style={[styles.dropdown]}
-              activeColor={colors.neutral600}
-              selectedTextStyle={styles.dropDownSelectedText}
-              iconStyle={styles.dropDownIcon}
-              data={transactionTypes}
-              maxHeight={300}
-              placeholderStyle={{ color: colors.neutral350 }}
-              labelField="label"
-              valueField="value"
-              placeholder="Select a type"
-              containerStyle={styles.dropDownContainerStyle}
-              itemContainerStyle={styles.dropDownItemContainer}
-              value={transaction.category}
-              itemTextStyle={styles.dropDownItemText}
-              onChange={(item) => {
-                setTransaction({ ...transaction, category: item });
-              }}
-            />
+        <ScrollView style={{ flex: 1 }}>
+          <View style={styles.form}>
+            <View style={styles.inputContainer}>
+              <Typo size={14} color={colors.neutral300}>
+                Type :
+              </Typo>
+              <Dropdown
+                style={[styles.dropdown]}
+                activeColor={colors.neutral600}
+                selectedTextStyle={styles.dropDownSelectedText}
+                iconStyle={styles.dropDownIcon}
+                data={transactionTypes}
+                maxHeight={300}
+                placeholderStyle={{ color: colors.neutral350 }}
+                labelField="label"
+                valueField="value"
+                placeholder="Select a type"
+                containerStyle={styles.dropDownContainerStyle}
+                itemContainerStyle={styles.dropDownItemContainer}
+                value={transaction.type}
+                itemTextStyle={styles.dropDownItemText}
+                onChange={(item) => {
+                  console.log("item", item);
+                  setTransaction({ ...transaction, type: item.value });
+                }}
+              />
+            </View>
+            {transaction.type === "expense" && (
+              <View style={styles.inputContainer}>
+                <Typo size={14} color={colors.neutral300}>
+                  Expense Category :
+                </Typo>
+                <Dropdown
+                  style={[styles.dropdown]}
+                  activeColor={colors.neutral600}
+                  selectedTextStyle={styles.dropDownSelectedText}
+                  iconStyle={styles.dropDownIcon}
+                  data={Object.values(expenseCategories)}
+                  maxHeight={300}
+                  placeholderStyle={{ color: colors.neutral350 }}
+                  labelField="label"
+                  valueField="value"
+                  placeholder="Select an expense categoty"
+                  containerStyle={styles.dropDownContainerStyle}
+                  itemContainerStyle={styles.dropDownItemContainer}
+                  value={transaction.category}
+                  itemTextStyle={styles.dropDownItemText}
+                  onChange={(item) => {
+                    setTransaction({
+                      ...transaction,
+                      category: item.value || "",
+                    });
+                  }}
+                />
+              </View>
+            )}
+            <View style={styles.inputContainer}>
+              <Typo size={14} color={colors.neutral300}>
+                Wallet Id :
+              </Typo>
+              <Dropdown
+                style={[styles.dropdown]}
+                activeColor={colors.neutral600}
+                selectedTextStyle={styles.dropDownSelectedText}
+                iconStyle={styles.dropDownIcon}
+                data={wallets.map((wallet) => ({
+                  label: `${wallet.name}($${wallet.amount})`,
+                  value: wallet.id,
+                }))}
+                maxHeight={300}
+                placeholderStyle={{ color: colors.neutral350 }}
+                labelField="label"
+                valueField="value"
+                placeholder="Select a type"
+                containerStyle={styles.dropDownContainerStyle}
+                itemContainerStyle={styles.dropDownItemContainer}
+                value={transaction.walletId}
+                itemTextStyle={styles.dropDownItemText}
+                onChange={(item) => {
+                  setTransaction({
+                    ...transaction,
+                    walletId: item.value || "",
+                  });
+                }}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <Typo size={14} color={colors.neutral300}>
+                Date :
+              </Typo>
+
+              {!showDatePicker && (
+                <TouchableOpacity
+                  style={styles.inputDate}
+                  onPress={() => setShowDatePicker(true)}
+                >
+                  <Typo size={12}>
+                    {(transaction.date as Date).toLocaleDateString()}
+                  </Typo>
+                </TouchableOpacity>
+              )}
+              {showDatePicker && (
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={transaction.date as Date}
+                  mode="date"
+                  display="calendar"
+                  onChange={(_, date) => {
+                    setTransaction({
+                      ...transaction,
+                      date: (transaction.date || date) as Date,
+                    });
+                    setShowDatePicker(false);
+                  }}
+                />
+              )}
+            </View>
+            <View style={styles.inputContainer}>
+              <Typo size={14} color={colors.neutral300}>
+                Amount :
+              </Typo>
+              <Input
+                placeholder="Amount"
+                value={transaction.amount?.toString()}
+                keyboardType="numeric"
+                onChangeText={(text) =>
+                  setTransaction({
+                    ...transaction,
+                    amount: Number(text.replace(/[^0-9]/g, "")) || 0,
+                  })
+                }
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Typo size={14} color={colors.neutral300}>
+                  Description{" "}
+                </Typo>
+                <Typo size={14} color={colors.neutral500}>
+                  (optional):
+                </Typo>
+              </View>
+              <Input
+                multiline={true}
+                onChangeText={(text) =>
+                  setTransaction({ ...transaction, description: text })
+                }
+                textAlignVertical="top"
+                placeholder="Description"
+                containerStyle={{ height: verticalScale(100) }}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Typo size={14} color={colors.neutral300}>
+                  Receipt{" "}
+                </Typo>
+                <Typo size={14} color={colors.neutral500}>
+                  (optional):
+                </Typo>
+              </View>
+              <UploadImage
+                file={walletData.image}
+                onSelect={onSelect}
+                onClear={onClear}
+              />
+            </View>
           </View>
-          <View style={styles.inputContainer}>
-            <Typo size={14} color={colors.neutral300}>
-              Wallet Id :
-            </Typo>
-            <Dropdown
-              style={[styles.dropdown]}
-              activeColor={colors.neutral600}
-              selectedTextStyle={styles.dropDownSelectedText}
-              iconStyle={styles.dropDownIcon}
-              data={wallets.map((wallet) => ({
-                label: `${wallet.name}($${wallet.amount})`,
-                value: wallet.id,
-              }))}
-              maxHeight={300}
-              placeholderStyle={{ color: colors.neutral350 }}
-              labelField="label"
-              valueField="value"
-              placeholder="Select a type"
-              containerStyle={styles.dropDownContainerStyle}
-              itemContainerStyle={styles.dropDownItemContainer}
-              value={transaction.walletId}
-              itemTextStyle={styles.dropDownItemText}
-              onChange={(item) => {
-                setTransaction({ ...transaction, walletId: item || "" });
-              }}
-            />
-          </View>
-          <View style={styles.inputContainer}>
-            <Typo size={14} color={colors.neutral300}>
-              Wallet Icon :
-            </Typo>
-            <UploadImage
-              file={walletData.image}
-              onSelect={onSelect}
-              onClear={onClear}
-            />
-          </View>
-        </View>
+        </ScrollView>
 
         <View style={styles.footer}>
           {oldWallet?.id && !isLoading && (
@@ -195,7 +317,7 @@ const TransactionModal = () => {
             style={{ flex: 1, width: "auto" }}
           >
             <Typo color={colors.neutral900} fontWeight={600}>
-              {oldWallet?.id ? "Update Wallet" : "Add wallet"}
+              {oldWallet?.id ? "Update Trasaction" : "Add Transaction"}
             </Typo>
           </Button>
         </View>
@@ -212,7 +334,8 @@ const styles = StyleSheet.create({
   form: {
     flex: 1,
     marginTop: verticalScale(30),
-    gap: spacingY._20,
+    paddingBottom: 30,
+    gap: 30,
   },
   inputContainer: { gap: spacingY._7 },
   footer: {
@@ -255,6 +378,15 @@ const styles = StyleSheet.create({
     borderRadius: radius._10,
   },
   activeItem: {},
+  inputDate: {
+    height: verticalScale(50),
+    borderColor: colors.neutral500,
+    borderWidth: 1,
+    borderRadius: radius._10,
+    paddingHorizontal: spacingX._15,
+
+    justifyContent: "center",
+  },
 });
 
 export default TransactionModal;
